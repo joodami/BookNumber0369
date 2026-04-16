d464e94d8bac827f9acbcab8078fed876ebed28b
joodami <joodami25@gmail.com>
2026-04-16 09:03:16 +0700

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function login(){
 
     if(res.length){
       userEl.value = res[0][1];
-      userformEl.classList.remove("invisible");
+      userformEl.classList.remove("d-none");
       document.body.classList.add("has-userform"); // ⭐ เพิ่ม
       post({action:"addOnline", name:res[0][1]});
     } else {
